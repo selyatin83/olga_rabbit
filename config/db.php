62343407c78fb6bc1da3311eac4d78fce93a6c53
Olga Marinina <youarewonderful@yandex.ru>
2022-11-29 21:44:59 +0700

    Сконфигурировала БД

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=db;dbname=buyAndSell',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root_password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
