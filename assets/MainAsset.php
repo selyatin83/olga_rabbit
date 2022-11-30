57ba93f3ccc0490e6bb49c6dae8dfa8792ba5c08
Olga Marinina <youarewonderful@yandex.ru>
2022-11-30 09:03:05 +0700

    Настроила главный asset для приложения

---
+++ assets/MainAsset.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class MainAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/style.min.css'
+    ];
+    public $js = [
+        'js/vendor.js',
+        'js/main.js'
+    ];
+    public $depends = [
+    ];
+}
